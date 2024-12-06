37106a15e2da2276b4793e4d8add64797d65e987
JaynesN <jaynesn763@my.clevelandcc.edu>
2024-12-06 09:45:49 -0500

    Add files via upload

---
+++ JavascriptP3/script.js
@@ -0,0 +1,8 @@
+function changeBG() {
+    document.getElementById("imga").src = "https://res.cloudinary.com/dybspc2v2/image/upload/v1733495705/20241206_093354_gib71t.jpg";
+    
+  }
+  
+  function changeBack() {
+      document.getElementById("imga").src = "https://res.cloudinary.com/dybspc2v2/image/upload/v1726233021/20240913_085327_qmvf7v.jpg";
+  }
